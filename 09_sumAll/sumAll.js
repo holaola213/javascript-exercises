b93fe2a40020efb579d70a043e7d16201ccabb8c
holaola213 <holaola213@gmail.com>
2025-09-25 22:12:53 -0700

    Finished 09
    Had to learn Number.isInteger();

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if (Number.isInteger(first) && Number.isInteger(second) && first >= 0 && second >= 0){
+        const array = [first, second]; 
+        array.sort((a,b) => a - b);
+        let total = 0;
+        for(let i = array[0]; i < array[1]; i++){
+            total += i;
+        }
+        return total + array[1];
+    }
+    else{
+        return "ERROR";
+    }
 
 };
 
